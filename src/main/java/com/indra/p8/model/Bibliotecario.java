7dc062de8d540a7c88270b8ace8721c488256ca5
igramiro <ivangonzalezramiro@gmail.com>
2025-12-03 15:35:16 +0100

    Changes by acardenal

---
+++ src/main/java/com/indra/p8/model/Bibliotecario.java
@@ -0,0 +1,24 @@
+package com.indra.p8.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bibliotecarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bibliotecario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+}
