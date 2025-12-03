691a0069d29ed3fa153cb75db9e4b25da99730db
igramiro <ivangonzalezramiro@gmail.com>
2025-12-03 15:36:36 +0100

    Espero que ya

--- src/main/java/com/indra/p8/model/Bibliotecario.java
+++ src/main/java/com/indra/p8/model/Bibliotecario.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Bibliotecario {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
